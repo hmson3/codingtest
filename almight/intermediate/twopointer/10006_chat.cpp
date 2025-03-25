04b09462aedbb0153b5ed68e93aa471d52a6fe59
Son_hyeongmin <shm0924@unist.ac.kr>
2025-03-25 15:37:06 +0900

    250325

--- almight/intermediate/twopointer/10006_chat.cpp
+++ almight/intermediate/twopointer/10006_chat.cpp
@@ -1,63 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int countNiceFragments(int n, int m, vector<int>& l, vector<int>& c, vector<int>& a) {
-    unordered_map<int, int> target_count, current_count;
-    long long window_size = 0, match_count = 0;
+long long int arr[1000001];
+long long int cnt[1000001] = {0};
+long long int target_cnt[1000001] = {0};
+long long int l[1000001] = {0};
+bitset<1000001> cmp;
+int c = 0;
 
-    // 목표 패턴 저장
-    for (int i = 0; i < m; i++) {
-        target_count[c[i]] = l[i];
-        window_size += l[i];
-        if(window_size > 1000001) {
-            cout << 0;
-            return 0;
-        }
-    }
+void add(int ii) {
+    if (cnt[ii] == target_cnt[ii]) { cmp[ii] = 0; c--; }
+    cnt[ii]++;
+    if (cnt[ii] == target_cnt[ii]) { cmp[ii] = 1; c++; }
+}
 
-    // 첫 윈도우 초기화
-    for (int i = 0; i < window_size; i++) {
-        current_count[a[i]]++;
-    }
-
-    // 첫 윈도우가 정답인지 확인
-    if (current_count == target_count) {
-        match_count++;
-    }
-
-    // 슬라이딩 윈도우 이동
-    for (int i = window_size; i < n; i++) {
-        // 새 요소 추가
-        current_count[a[i]]++;
-
-        // 가장 오래된 요소 제거
-        int remove_index = i - window_size;
-        if (--current_count[a[remove_index]] == 0) {
-            current_count.erase(a[remove_index]);
-        }
-
-        // 패턴 검사
-        if (current_count == target_count) {
-            match_count++;
-        }
-    }
-
-    return match_count;
+void del(int ii) {
+    if (cnt[ii] == target_cnt[ii]) { cmp[ii] = 0; c--; }
+    cnt[ii]--;
+    if (cnt[ii] == target_cnt[ii]) { cmp[ii] = 1; c++; }
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m;
+    long long int n, m;
     cin >> n >> m;
-
-    vector<int> l(m), c(m), a(n);
-
-    for (int i = 0; i < m; i++) cin >> l[i];
-    for (int i = 0; i < m; i++) cin >> c[i];
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    cout << countNiceFragments(n, m, l, c, a) << endl;
-
-    return 0;
+    long long int L = 0;
+    for(int i = 0; i < m; i++) {
+        cin >> l[i];
+        L += l[i];
+    }
+    for(long long int i = 0; i < m; i++) {
+        int num;
+        cin >> num;
+        target_cnt[num] = l[i];
+    }
+    for(long long int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    if(L > n) {
+        cout << 0;
+        return 0;
+    }
+    for(long long int i = 0; i < L; i++) {
+        cnt[arr[i]]++;
+    }
+    cmp.reset();
+    for(long long int i = 0; i <= n; i++) {
+        if(cnt[i] == target_cnt[i]) {
+            cmp[i] = 1;
+            c++;
+        }
+    }
+    int ans = 0;
+    if(c == n + 1) {
+        ans++;
+    }
+    for(long long int i = L; i < n; i++) {
+        int left = arr[i - L];
+        int right = arr[i];
+        del(left);
+        add(right);
+        if(c == n + 1) {
+            ans++;
+        }
+    }
+    cout << ans;
 }
