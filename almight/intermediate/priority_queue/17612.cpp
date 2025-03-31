a29d60fe1b40f72ae6cac3bc04d7bdb1e2497341
Son_hyeongmin <shm0924@unist.ac.kr>
2025-03-31 22:26:16 +0900

    250331

--- almight/intermediate/priority_queue/17612.cpp
+++ almight/intermediate/priority_queue/17612.cpp
@@ -2,54 +2,59 @@
 using namespace std;
 
 int n,k;
+int now = 0;
 struct customer {
     int id;
     int time;
     int counter;
 };
-bool cmp(customer a, customer b) {
-    if(a.time == b.time) return a.counter > b.counter;
-    else return a.time < b.time;
-} 
-priority_queue<customer,vector<customer>,decltype(&cmp)> k1;
+vector<int> ex;
+struct cmp {
+    bool operator()(const customer& a, const customer& b) const {
+        if(a.time == b.time) return a.counter > b.counter;
+        else return a.time > b.time;
+    }
+};
+priority_queue<customer,vector<customer>,cmp> busy;
+priority_queue<int> wait;
+
+void exitcounter() {
+    customer c = busy.top(); busy.pop();
+    now = c.time;
+    wait.push(c.counter);
+    ex.push_back(c.id);
+}
+
+void entercounter(int id, int w) {
+    if(wait.empty()) {
+        exitcounter();
+        while(!busy.empty() && busy.top().time == now) exitcounter();
+    }
+    int c = wait.top(); wait.pop();
+    customer cc = {id,w+now,c};
+    busy.push(cc);
+
+}
+
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> k;
+    for(int i = 1; i <= k; i++) wait.push(i);
     for(int i = 0; i < n; i++) {
-        
+        int id,w;
+        cin >> id >> w;
+        entercounter(id,w);
     }
-    if(n > k) {
+    
 
+    while(!busy.empty()) exitcounter();
+    long long ans = 0;
+    long long i = 1;
+    for(auto e : ex) {
+        ans += i++*e;
     }
-    else {
-        for(int i = k; k > 0; i--) {
-
-        }
-    }
-    int ii;
-    cin >> ii;
-    le.push(ii);
-    cout << ii << '\n';
-    for(int i = 1; i < n; i++) {
-        int k;
-        cin >> k;
-        if(k > le.top()) {
-            ri.push(k);
-        }
-        else {
-            le.push(k);
-        }
-        if(le.size() < ri.size()) {
-            le.push(ri.top());
-            ri.pop();
-        }
-        else if(le.size() > ri.size() + 1) {
-            ri.push(le.top());
-            le.pop();
-        }
-        cout << le.top() << '\n';
-    }
+    cout << ans;
 }
